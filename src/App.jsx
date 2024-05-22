3b175a2242ac148bb67dcf1eaaff469a273c4076
Makar Bondar <makar.bondar@rt.ru>
2024-05-22 12:14:42 +0300

    Сортировка массива

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import "./style.css";
+import { listOfSubdivisions } from "./store/listOfSubdivisions";
+import SubdivisionList from "./components/SubdivisionList";
 
 function App() {
     return (
         <>
+            <SubdivisionList listOfSubdivisions={listOfSubdivisions} />
         </>
     );
 }
---
+++ src/components/SubdivisionList.jsx
@@ -0,0 +1,11 @@
+﻿import "../style.css";
+
+const SubdivisionList = ({ listOfSubdivisions }) => {
+
+    const sortedSubdivisions = listOfSubdivisions.sort((a, b) =>
+        a.code.localeCompare(b.code)
+    );
+    console.log(sortedSubdivisions)
+};
+
+export default SubdivisionList;
