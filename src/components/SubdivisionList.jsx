ebadd5c000d0dd1257e8434c109b060118a0bd34
Makar Bondar <makar.bondar@rt.ru>
2024-05-22 12:38:11 +0300

    Отображение отсортированных подразделений на странице

--- src/components/SubdivisionList.jsx
+++ src/components/SubdivisionList.jsx
@@ -1,11 +1,24 @@
 ﻿import "../style.css";
 
 const SubdivisionList = ({ listOfSubdivisions }) => {
-
+    // Сортировка подразделений по коду
     const sortedSubdivisions = listOfSubdivisions.sort((a, b) =>
         a.code.localeCompare(b.code)
     );
-    console.log(sortedSubdivisions)
+    //console.log(sortedSubdivisions)
+
+    return (
+        <div className="subdivision-list">
+            {sortedSubdivisions.map((subdivision) => (
+                <div className="subdivision">
+                    <div className="subdivision-code">{subdivision.code}</div>
+                    <div className="subdivision-name">{subdivision.name}</div>
+                    <div className="status-indicator">Статус</div>
+                    <div className="arrow">*</div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default SubdivisionList;
