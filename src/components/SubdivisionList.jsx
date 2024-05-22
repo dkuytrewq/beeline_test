f6c3a878d9fe6541838b9fe294ed4d75b24c8325
Makar Bondar <makar.bondar@rt.ru>
2024-05-22 12:53:25 +0300

    Добавлен вывод инфы по сотрудникам и должностям

--- src/components/SubdivisionList.jsx
+++ src/components/SubdivisionList.jsx
@@ -10,12 +10,23 @@ const SubdivisionList = ({ listOfSubdivisions }) => {
     return (
         <div className="subdivision-list">
             {sortedSubdivisions.map((subdivision) => (
-                <div className="subdivision">
-                    <div className="subdivision-code">{subdivision.code}</div>
-                    <div className="subdivision-name">{subdivision.name}</div>
-                    <div className="status-indicator">Статус</div>
-                    <div className="arrow">*</div>
+                <div key={subdivision.id} className="subdivision-container">
+                    <div className="subdivision">
+                        <div className="subdivision-code">{subdivision.code}</div>
+                        <div className="subdivision-name">{subdivision.name}</div>
+                        <div className="status-indicator">Статус</div>
+                        <div className="arrow">*</div>
+                    </div>
+                    <div className="positions">
+                        {subdivision.listOfPositions.map((position) => (
+                            <div key={position.id} className="position">
+                                <div className="position-name">{position.name}</div>
+                                <div className="collaborator-fullname">{position.collaboratorFullname}</div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
+
             ))}
         </div>
     );
